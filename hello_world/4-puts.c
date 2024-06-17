6e016a04c526545d2f1a1040d124b5509e2ea986
Yannis95200 <ranguin.yannis98@gmail.com>
2024-06-17 13:11:36 +0000

    test1

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void) {
-        //puts the programme// 
+        /*puts the programme*/
 
        	puts ( "Programming is like building a multilingual puzzle" );
 return 0;
