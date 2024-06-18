9d33ae850cdc8b6494ba15b7e078898d8caa91e3
Your Name <ranguin.yannis98@gmail.come>
2024-06-18 21:45:43 +0000

    test1

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: always 0 (success)
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+
+	putchar(num + '0');
+	if (num < 9)
+
+
+	putchar('\n');
+	return (0);
+}
