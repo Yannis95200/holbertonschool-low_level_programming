23a29f16b8da7ff1b25acfc165e08ca38271ea93
Your Name <ranguin.yannis98@gmail.come>
2024-06-18 18:53:09 +0000

    test1

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: always 0 (success)
+*/
+int main(void)
+{
+	char alphabet;
+
+	{
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	putchar (alphabet);
+	putchar ('\n');
+
+	for (alphabet = 'Z'; alphabet <= 'Z'; alphabet++)
+	putchar (alphabet);
+	putchar ('\n');
+	}
+	return (0);
+}
