849fced901907251510d24b1471d21154b80460f
Yannis95200 <ranguin.yannis98@gmail.com>
2024-06-18 13:52:43 +0000

    test1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 	}
 	else if (last == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n , last);
+		printf("Last digit of %d is %d and is 0\n", n, last);
 	}
 	else if (last < 6)
 	{
