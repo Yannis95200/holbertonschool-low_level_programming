332ad83ac09272bd71a09fe8551e746286a07428
Yannis95200 <ranguin.yannis98@gmail.com>
2024-06-18 14:25:52 +0000

    test1

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: always 0 (success)
+*/
+int main(void)
+{
+	char alphabet = 'a';
+
+	{
+	while (alphabet <= 'z')
+	putchar (alphabet);
+	alphabet ++;
+		
+	}
+
+	putchar ('\n');
+	return (0);
+}
