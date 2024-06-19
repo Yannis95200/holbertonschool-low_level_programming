8891c05cefeeaab32e342d5899c99335444ed7f6
Yannis95200 <ranguin.yannis98@gmail.com>
2024-06-19 12:12:42 +0000

    test1

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -7,10 +7,15 @@ int main(void)
 {
 	char num;
 
-	for (num = 0; num < 10; num++)
-		putchar(num);
+	for (num = '0'; num <= '9'; num++)
+	{
+	putchar(num);
+	if (num == '9')
+	continue;
 
 	putchar(',');
+	putchar(' ');
+	}
 	putchar('\n');
 	return (0);
 }
