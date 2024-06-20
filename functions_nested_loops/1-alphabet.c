bbe05a4dc0a317244a886ce5f65f8b265922c596
Yannis95200 <ranguin.yannis98@gmail.com>
2024-06-20 19:40:57 +0000

    test1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -5,17 +5,18 @@
 *
 */
 
-void print_alphabet(void);
+void print_alphabet(void)
 
 {
 	char alphabet;
 
-	{
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+
+
+	{
 		_putchar(alphabet);
 
 	}
 
 	_putchar('\n');
-
 }
