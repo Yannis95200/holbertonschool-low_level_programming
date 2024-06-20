b135c664d960594cacb3f8e10969065d7c520ae3
Yannis95200 <ranguin.yannis98@gmail.com>
2024-06-20 19:25:56 +0000

    test1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* main - Entry point
+* Return: always 0 (success)
+*
+*/
+
+void print_alphabet(void)
+
+{
+	char alphabet;
+
+	{
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		_putchar(alphabet);
+
+	}
+
+	_putchar('\n');
+
+}
