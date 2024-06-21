c8f6a9b69170e5f6531d2d8aa241b38f26fbb13d
Yannis95200 <ranguin.yannis98gmail.comm>
2024-06-21 11:44:16 +0000

    test1

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _abs -check if int will find an absolute
+* @n: charater
+* Return: 0
+*/
+
+int _abs(int n)
+{
+
+	if (n >= 0)
+	{
+	return (n);
+	}
+	else
+	{
+	return (-n);
+	}
+}
