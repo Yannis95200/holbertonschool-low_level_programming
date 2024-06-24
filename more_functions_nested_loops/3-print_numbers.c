368e644b5d921c571e530105088121622dbfbead
Yannis95200 <ranguin.yannis98@gmail.com>
2024-06-24 12:58:51 +0000

    test1

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
-* print_numbers -prints the numbers, from 0 to 9
-* Return: 0
-*/
+ * print_numbers -prints the numbers, from 0 to 9
+ * Return: 0
+ */
 void print_numbers(void)
 {
+	int num;
+
 	for (num = 0; num < 10; num++)
 		_putchar(num + '0');
 
 	_putchar('\n');
-
-	return (0);
 }
