edbf9c80a57ef1d5d260ade6e2b3814723a73342
Yannis95200 <ranguin.yannis98@gmail.com>
2024-06-24 14:53:39 +0000

    test1

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -10,10 +10,15 @@ void more_numbers(void)
 	int n;
 
 	for (n = 1; n <= 10; n++)
+	{
+		for (num = 0; num  <= 14; num++)
+		{
+			if (num >= 14)
+			{
+				_putchar ('0' + num % 10);
+				_putchar ('0' + num / 10);
+			}
+		}
+	}	_putchar('\n');
 
-	for (num = 0; num  <= 14; num++)
-	{		_putchar((num % 10) + '0');
-			_putchar((n % 10) + '0');
-			_putchar('\n');
-	}
 }
