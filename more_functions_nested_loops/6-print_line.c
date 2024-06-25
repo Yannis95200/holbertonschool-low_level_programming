33dfa2bbdfc7d3bd16b6782c3dc2c3911747a642
Your Name <you@example.com>
2024-06-25 16:48:14 +0000

    test1

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -7,18 +7,12 @@
 
 void print_line(int n)
 {
-	int a = 0;
-
-	if (n > 0)
 	{
-		while (a <= n)
-		{
-			_putchar('_');
-			a++;
-		}
-
-	_putchar('\n');
+	while (n-- > 0)
+	{
+	_putchar('_');
 	}
-	else
 	_putchar('\n');
+
+	}
 }
