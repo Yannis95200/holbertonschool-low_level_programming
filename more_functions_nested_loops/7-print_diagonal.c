28ca40fb351cdcb3e090bd1932482e36bd01edeb
Your Name <you@example.com>
2024-06-25 14:50:20 +0000

    test1

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+*print_diagonal -draws a diagonal line on the terminal.
+*@n: is char
+*/
+void print_diagonal(int n)
+{
+	int a = 0;
+	int b = 0;
+
+	if (n > 0)
+	{
+		while (a < n)
+		{
+			while (b < a)
+			{
+				_putchar(' ');
+				b++;
+			}
+			a++;
+			b = 0;
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+	_putchar('\n');
+}
