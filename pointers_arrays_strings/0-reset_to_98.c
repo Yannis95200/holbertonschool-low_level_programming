bb06a40825b4efb7e98ba65ed47da95b4fc97c4b
Your Name <you@example.com>
2024-06-26 11:48:20 +0000

    test1

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+*reset_to_98 -Write a function that takes a pointer
+*@n: is char
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <unistd.h>
+
+
+void reset_to_98(int *n);
+#endif
