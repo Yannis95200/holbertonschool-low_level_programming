e8b12bb676f09ac11ac1ef3e0b55b8f6b8e593be
Your Name <you@example.com>
2024-06-29 17:10:55 +0000

    test1

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcmp - function that compares two strings.
+ * @s1:pointer
+ * @s2: pointer
+ * Return: s1 - s2
+ */
+
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[1])
+		{
+			return (s1[1] - s2[i]);
+		}
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
