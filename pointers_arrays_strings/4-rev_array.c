bb480c8b587f5a933c93a28d42d8648c9632d603
Your Name <you@example.com>
2024-06-29 18:07:56 +0000

    tes1

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * reverse_array - function that reverses the content of an array of integers
+ * @a: pointer
+ * @n: variable
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, temp;
+
+	for (i = 0; i < n / 2; i++)
+	{
+	temp = a[i];
+	a[i] = a[n - i - 1];
+	a[n - i - 1] = temp;
+	}
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 
 #include <unistd.h>
 #include <stdio.h>
+
 int _putchar(char c);
 void swap_int(int *a, int *b);
 void reset_to_98(int *n);
@@ -19,4 +20,5 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
 #endif
