fbb53a96053604108a09ab7152fe8da9b35acb4b
Your Name <you@example.com>
2024-06-29 17:20:36 +0000

    test1

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -14,11 +14,13 @@ int _strcmp(char *s1, char *s2)
 
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (s1[i] != s2[1])
-		{
-			return (s1[1] - s2[i]);
-		}
-		i++;
+
+	if (s1[i] != s2[i])
+	{
+	return (s1[i] - s2[i]);
 	}
+	i++;
+	}
+
 	return (s1[i] - s2[i]);
 }
