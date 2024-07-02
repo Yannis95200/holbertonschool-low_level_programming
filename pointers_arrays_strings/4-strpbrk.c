8cd9e9e9b2d87ccdb52fde83c0fb177d40e5ba8e
Your Name <you@example.com>
2024-07-02 08:51:18 +0000

    test1

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strpbrk - The strpbrk function locates the first occurrence in the string
+ * @s:pointer
+ * @accept:character
+ * Return: count
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+			}
+
+		}
+	}
+
+	return (NULL);
+}
