570ddfb9d1d52e2b7fb6ad2c45e3c419cfc3647a
Your Name <you@example.com>
2024-07-03 13:48:07 +0000

    test1

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main -program that prints its name
+ *@argc: variable
+ *@argv: pointers
+ *Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("My name is yannis: %s\n", argv[0]);
+	{
+	return (0);
+	}
+}
---
+++ argc_argv/main.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <unistd.h>
+#include <stdio.h>
+
+int _putchar(char c);
+int main(int argc, char *argv[]);
+#endif
+
