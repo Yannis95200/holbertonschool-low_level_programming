0612b1b18d78c9e2224b79909b3e5eccd39281bc
Your Name <you@example.com>
2024-07-03 09:15:57 +0000

    test1

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -7,14 +7,14 @@
 */
 int factorial(int n)
 {
-	if (n < 0)
-	{
-		return (-1);
-	}
 	if (n == 0 || n == 1)
 	{
 		return (1);
 	}
-
-	return (n * factorial(n - 1));
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+		return (n * factorial(n - 1));
 }
