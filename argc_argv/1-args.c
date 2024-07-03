9498f801277d54949396f7fa744e4d3407fb584a
Your Name <you@example.com>
2024-07-03 14:24:15 +0000

    test1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main -program that prints its name
+ *@argc: variable
+ *@argv: pointers
+ *Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)*argv;
+	printf("%d\n", argc -1);
+	{
+	return (0);
+	}
+}
