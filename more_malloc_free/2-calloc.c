b6615034dcea830326e68498e92a5434ef45fe63
Your Name <you@example.com>
2024-07-09 14:37:00 +0000

    _calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -8,27 +8,28 @@
  * @size: Taille en octets de chaque element
  * Return: point vers le tab allouer et initialise a zero
  */
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int total_size = nmemb * size;
+	unsigned int total = nmemb * size;
 	unsigned int i;
-	void *ptr = malloc(total_size);
-	char *char_ptr = (char *)ptr;
+	char *ptr;
 
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
 
+	ptr = malloc(nmemb * size);
+
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
 
-	for (i = 0; i < total_size; i++)
+	for (i = 0; i < total; i++)
 	{
-		char_ptr[i] = 0;
-
+		ptr[i] = 0;
 	}
 
 	return (ptr);
