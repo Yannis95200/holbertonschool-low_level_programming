1606e6368e80eb3337f745d55e74afe5f5bbfe89
Your Name <you@example.com>
2024-07-10 17:45:09 +0000

    Define a new type struct dog with the following elements

---
+++ structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+ * struct dog - variable pour d√crire un chien
+ * @name: nom du chien
+ * @age: age du chien
+ * @owner: le propri√taire du chien
+ */
+
+
+struct dog
+{
+
+	char *name;
+	float age;
+	char *owner;
+}
+
+
+
+#endif
