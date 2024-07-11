8d5a168c31dfb78dd4ef959aa522d3eb41c7740a
Your Name <you@example.com>
2024-07-11 13:42:04 +0000

    Write a function that frees dogs.wq

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,6 +1,6 @@
 #include "dog.h"
 #include "stdio.h"
-
+#include <stdlib.h>
 /**
  * free_dog -Frees the memory allocated for a struct dog type structure.
  * @d:Pointer to the dog structure to release
@@ -9,10 +9,10 @@
 void free_dog(dog_t *d)
 {
 	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 
-	free(d->name);
-	free(d->age);
-	free(d->owner);
-
-	return (0);
 }
