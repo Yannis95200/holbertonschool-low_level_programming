7bd85f07eb69ee12637a6e1a78f2a07ed5c14859
Your Name <you@example.com>
2024-07-11 13:35:13 +0000

    Write a function that creates a new dog.wq

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -50,33 +50,39 @@ char *_strcpy(char *dest, char *src)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	char *name_copy, *owner_copy;
+
+	if (name == NULL || age < 0 || owner == NULL)
+	{
+		return (NULL);
+	}
 
 	new_dog = malloc(sizeof(dog_t));
 
 	if (new_dog == NULL)
+	{
 		return (NULL);
+	}
 
-	name_copy = malloc(strlen(name) + 1);
-	if (name_copy == NULL)
+	new_dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
+
+	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-	strcpy(name_copy, name);
 
-	owner_copy = malloc(strlen(owner) + 1);
-	if (owner_copy == NULL)
+	new_dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+
+	if (new_dog->owner == NULL)
 	{
-		free(name_copy);
+		free(new_dog->name);
 		free(new_dog);
+		return (NULL);
 	}
 
-	strcpy(owner_copy, owner);
-
-	new_dog->name = name_copy;
+	new_dog->name = _strcpy(new_dog->name, name);
 	new_dog->age = age;
-	new_dog->owner = owner_copy;
+	new_dog->owner = _strcpy(new_dog->owner, owner);
 
 	return (new_dog);
 }
