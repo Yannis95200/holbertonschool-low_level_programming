12c05458c3b2a24189daac7c6a332c456ac3d252
Your Name <you@example.com>
2024-07-15 14:25:25 +0000

    Write a function that searches for an integer.

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -5,4 +5,5 @@
 void array_iterator(int *array, size_t size, void (*action)(int));
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+int int_index(int *array, int size, int (*cmp)(int));
 #endif
