9f850fc74d2ccb88652255658503f5b2620c1c39
Your Name <you@example.com>
2024-07-15 14:33:57 +0000

    Write a function that searches for an integer.

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,15 +1,18 @@
 #include "function_pointers.h"
 #include <stdio.h>
 /**
- *array_iterator - Executes a function given as a parameter on each element
- *of an array.
- *@array: The array to iterate over.
- *@size: The size of the array.
- *@action: Pointer to a function that takes an int argument and returns void.
+ *int_index -Searches for an integer in an array.
+ *@array: The array to search through.
+ *@size: The number of elements in the array.
+ *@cmp: Pointer to a function that takes an
+ * int as an argument and returns an int
+ *Return: The index of the first element for which
+ *the cmp function does not return 0,
+ * or -1 if no element matches or if input parameters are invalid.
  */
 
-
 int int_index(int *array, int size, int (*cmp)(int))
+
 {
 	int i;
 
@@ -19,7 +22,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
-		return (i);
+			return (i);
 	}
 	return (-1);
 
