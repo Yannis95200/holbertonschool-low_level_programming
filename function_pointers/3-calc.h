2a80ccb593f4a54424b93eb1f561ff44118a8555
Your Name <you@example.com>
2024-07-15 15:58:30 +0000

    correction

---
+++ function_pointers/3-calc.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b)
+int op_mod(int a, int b);
+
+#endif
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,70 @@
+#include "function_pointers.h"
+#include "3-calc.h"
+#include <stdio.h>
+
+/**
+ * op_add - Adds two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The sum of a and b.
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+
+}
+
+/**
+ * op_sub - Subtracts the second integer from the first.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The difference of a and b.
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+
+}
+/**
+ * op_mul - Multiplies two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The product of a and b.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - Divides the first integer by the second.
+ * @a: The numerator.
+ * @b: The denominator.
+ *
+ * Return: The result of a divided by b.
+ *         Note: Division by zero is not handled here.
+ */
+
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - Computes the modulus of the first integer by the second.
+ * @a: The numerator.
+ * @b: The denominator.
+ *
+ * Return: The remainder of a divided by b.
+ *         Note: Modulus by zero is not handled here.
+ */
+
+
+int op_mod(int a, int b)i
+{
+	return (a % b);
+}
