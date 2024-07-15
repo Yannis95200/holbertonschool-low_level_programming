dbee248fa3b4d769a1f74b865607fe6d9e8369b6
Your Name <you@example.com>
2024-07-15 13:23:55 +0000

    Write a function that executes a function given as a parameter on each element of an array.

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -2,13 +2,7 @@
 #define HEADER_FILE
 
 #include <unistd.h>
-
-
-
-
-
-
-
+void array_iterator(int *array, size_t size, void (*action)(int));
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 #endif
