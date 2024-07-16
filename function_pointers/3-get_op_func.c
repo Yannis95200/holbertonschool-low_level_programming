b27c192d286664078d19d3d3633cd21f0bee8c08
Your Name <you@example.com>
2024-07-16 14:14:28 +0000

    Write a program that performs simple operations.

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -4,12 +4,6 @@
  * get_op_func - Selects the correct function to perform the operation
  *               requested by the user.
  * @s: The operator passed as argument.
- * Description: This function takes an operator
- * as a string and returns a pointer
- *	to the corresponding function that performs the operation.It
- *	searches through an array of
- *	structures containing operators andtheir corresponding functions.
- *
  * Return: A pointer to the function that corresponds to the operator given as
  *		a parameter. If no match is found, it returns NULL
  */
