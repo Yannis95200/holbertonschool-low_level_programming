1b7e31c4943ef263bb9554e0e3ba331609512537
Your Name <you@example.com>
2024-07-31 14:25:10 +0000

    Write a function that prints the binary representation of a number

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * print_binary - Prints the binary representation of a number.
+ * @n: The number to be printed in binary.
+ *
+ * Description: This function uses recursion to print the binary
+ * representation of a number.
+ */
+
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	{
+		print_binary(n >>  1);
+	}
+	_putchar((n & 1) + '0');
+}
